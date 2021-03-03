b232c9a3b5a1a986845bdfe5736cc2fc4521e4f8
i-k0n <19754246+i-k0n@users.noreply.github.com>
2021-03-03 14:09:02 -0600

    basic chat engine working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+      height="100vh"
+      projectID={process.env.REACT_APP_CHATENGINE_PROJECT_ID}
+      userName={process.env.REACT_APP_CHATENGINE_USER_NAME}
+      userSecret={process.env.REACT_APP_CHATENGINE_USER_SECRET}
+    />
   );
 }
 
