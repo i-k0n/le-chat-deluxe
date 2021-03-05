720c3f3bcce673e5f5fe11acaf5bbae3db1928ee
i-k0n <19754246+i-k0n@users.noreply.github.com>
2021-03-05 13:21:54 -0600

    added send message form, including img input

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { sendMessage, isTyping } from 'react-chat-engine'
 
-export default function MessageForm() {
+export default function MessageForm(props) {
+  const [value, setValue] = useState('')
+  const { chatId, creds } = props
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const text = value.trim()
+
+    if (text.length > 0) sendMessage(creds, chatId, { text })
+
+    setValue('')
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+
+    isTyping(props, chatId)
+  }
+
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: '' })
+  }
+
   return (
-    <div>
-      MessageForm
-    </div>
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input 
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          IMG
+        </span>
+        <input 
+          type="file"
+          multiple={false}
+          id="upload-button"
+          style={{ display: "none" }}
+          onChange={handleUpload}
+        />
+        <button type="submit" className="send-button">Submit</button>
+      </label>
+    </form>
   )
 }
