c67ac246270aedf8a037bd0f55cef0a2e4ca4fac
i-k0n <19754246+i-k0n@users.noreply.github.com>
2021-03-13 15:16:47 -0600

    begin custom chat settings panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine'
 
-import ChatFeed from './components/ChatFeed'
-import ChatList from './components/ChatList'
 import LoginForm from './components/LoginForm'
+import ChatList from './components/ChatList'
+import ChatFeed from './components/ChatFeed'
+import ChatSettings from './components/ChatSettings'
 
 function App() {
+  // if we don't have login credentials in localStorage, show Login form instead
   if (!localStorage.getItem('le-chat-deluxe-username')) return <LoginForm />
 
   return (
@@ -16,6 +18,7 @@ function App() {
       userSecret={localStorage.getItem('le-chat-deluxe-password')}
       renderChatList={(chatAppProps) => <ChatList {...chatAppProps} />}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      renderChatSettings={(chatAppProps) => <ChatSettings {...chatAppProps} />}
     />
   );
 }
